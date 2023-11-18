0c1bd1554327969f3346b716497b6d20ee16e4bc
AdrianGerman <adrian.german1019@gmail.com>
2023-11-18 09:17:04 -0600

    states

--- src/Components/BaseLayout.js
+++ src/Components/BaseLayout.js
@@ -6,5 +6,8 @@ export const BaseLayout = () => {
   const [state, send] = useMachine(bookingMachine);
 
   console.log("nuestra maquina", state);
+  console.log("matches true", state.matches("initial"));
+  console.log("matches false", state.matches("tickets"));
+  console.log("can", state.can("FINISH"));
   return <div>Hello world!</div>;
 };
