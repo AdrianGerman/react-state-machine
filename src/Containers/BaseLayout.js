16f447f0e2467d99e34befec5491f632914c885f
AdrianGerman <adrian.german1019@gmail.com>
2023-11-19 13:31:07 -0600

    structure

--- src/Containers/BaseLayout.js
+++ src/Containers/BaseLayout.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
 import bookingMachine from "../Machines/bookingMachine";
+import "./BaseLayout.css";
+import { StepsLayout } from "./StepsLayout";
+import { Nav } from "../Components/Nav";
 
 export const BaseLayout = () => {
   const [state, send] = useMachine(bookingMachine);
@@ -9,5 +12,10 @@ export const BaseLayout = () => {
   console.log("matches true", state.matches("initial"));
   console.log("matches false", state.matches("tickets"));
   console.log("can", state.can("FINISH"));
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <Nav />
+      <StepsLayout />
+    </div>
+  );
 };
