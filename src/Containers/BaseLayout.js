4f43a2c0106bcd09d74d8858adbc2c79aa54670e
AdrianGerman <adrian.german1019@gmail.com>
2023-11-20 08:19:19 -0600

    bug states resolved

--- src/Containers/BaseLayout.js
+++ src/Containers/BaseLayout.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
+import { Nav } from "../Components/Nav";
+import { StepsLayout } from "./StepsLayout";
 import bookingMachine from "../Machines/bookingMachine";
 import "./BaseLayout.css";
-import { StepsLayout } from "./StepsLayout";
-import { Nav } from "../Components/Nav";
 
 export const BaseLayout = () => {
   const [state, send] = useMachine(bookingMachine);
 
-  console.log("nuestra maquina", state);
-  console.log("matches true", state.matches("initial"));
-  console.log("matches false", state.matches("tickets"));
-  console.log("can", state.can("FINISH"));
+  console.log("nuestra maquina", state.value);
+
   return (
-    <div>
-      <Nav />
-      <StepsLayout />
+    <div className="BaseLayout">
+      <Nav state={state} send={send} />
+      <StepsLayout state={state} send={send} />
     </div>
   );
 };
