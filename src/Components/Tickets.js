fa7d229eb6299d63f680a4f46500951fe58d5692
AdrianGerman <adrian.german1019@gmail.com>
2023-11-20 08:15:48 -0600

    tickets page

--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -1,1 +1,25 @@
-export const Tickets = ({ state, send }) => {};
+import React from "react";
+import "./Tickets.css";
+
+export const Tickets = ({ state, send }) => {
+  const finish = () => {
+    send("FINISH");
+  };
+
+  return (
+    <div className="Tickets">
+      <p className="Tickets-description description">
+        Gracias por volar con nosotros
+      </p>
+      <div className="Tickets-ticket">
+        <div className="Tickets-country">Mexico</div>
+        <div className="Tickets-passengers">
+          <span>✈</span>
+        </div>
+      </div>
+      <button onClick={finish} className="Tickets-finalizer button">
+        Finalizar
+      </button>
+    </div>
+  );
+};
