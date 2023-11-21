6610372a4ee15646db7656586343e8f841e4e394
AdrianGerman <adrian.german1019@gmail.com>
2023-11-21 08:11:49 -0600

    actions

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Search.css";
 
 export const Search = ({ send }) => {
   const [flight, setFlight] = useState("");
--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,33 +1,47 @@
 import { createMachine } from "xstate";
 
-const bookingMachine = createMachine({
-  predictableActionArguments: true,
-  id: "Comprar boletos de avión",
-  initial: "initial",
-  states: {
-    initial: {
-      on: {
-        START: "search",
+const bookingMachine = createMachine(
+  {
+    predictableActionArguments: true,
+    id: "Comprar boletos de avión",
+    initial: "initial",
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "imprimirInicio",
+          },
+        },
       },
-    },
-    search: {
-      on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
+      search: {
+        entry: "imprimirEntrada",
+        exit: "imprimirSalida",
+        on: {
+          CONTINUE: "passengers",
+          CANCEL: "initial",
+        },
       },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: "initial",
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: "initial",
+        },
       },
-    },
-    tickets: {
-      on: {
-        FINISH: "initial",
+      tickets: {
+        on: {
+          FINISH: "initial",
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      imprimirInicio: () => console.log("Imprimir inicio"),
+      imprimirEntrada: () => console.log("Imprimir entrada a search"),
+      imprimirSalida: () => console.log("Imprimir salida del search"),
+    },
+  }
+);
 
 export default bookingMachine;
